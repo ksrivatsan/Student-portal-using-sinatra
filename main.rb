00fbd4706695b83ec013f7ab4f4223f901bba8c9
ksrivatsan <kauvas@gmail.com>
2015-05-19 18:33:49 -0700

    First commit

---
+++ main.rb
@@ -0,0 +1,68 @@
+require 'sinatra'
+require 'slim'
+require 'sass'
+require './student'
+
+configure do
+  enable :sessions
+  set :username, 'frank'
+  set :password, 'sinatra'
+end
+
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/student.db")
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+  # DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://ahybktfzdxclth:F5eGwK8G_3g0fzH_cGwJoRcQI1@ec2-23-23-126-39.compute-1.amazonaws.com:5432/d5t2hvcqltlg4g')
+end
+
+get('/styles.css'){ scss :styles }
+
+get '/' do
+  slim :login
+end
+
+get '/home' do
+  slim :home
+end
+
+get '/about' do
+  @title = "All About This Website"
+  slim :about
+end
+
+get '/contact' do
+  slim :contact
+end
+
+not_found do
+  slim :not_found
+end
+
+get '/login' do
+  slim :login
+end
+
+post '/login' do
+  if params[:username] == settings.username && params[:password] == settings.password
+    session[:admin] = true
+    redirect to('/students')
+  else
+    slim :login
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect to('/login')
+end
+
+get '/set/:name' do
+  session[:name] = params[:name]
+end
+
+get '/get/hello' do
+  "Hello #{session[:name]}"
+end
---
+++ student.rb
@@ -0,0 +1,55 @@
+require 'dm-core'
+require 'dm-migrations'
+
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/student.db")
+
+class Student
+
+  include DataMapper::Resource
+  
+  property :id, Serial
+  property :first_name, String
+  property :last_name, String
+  property :age, Integer
+  property :phone_number, String
+  property :address, Text
+
+end
+
+DataMapper.finalize
+
+get '/students' do
+  @students = Student.all
+  slim :students
+end
+
+get '/students/new' do
+  @student = Student.new
+  slim :new_student
+end
+
+get '/students/:id' do
+	@student = Student.get(params[:id])
+	slim :show_student
+end
+
+get '/students/:id/edit' do
+  @student = Student.get(params[:id])
+  slim :edit_student
+end
+
+post '/students' do  
+  student = Student.create(params[:student])
+  redirect to("/students/#{student.id}")
+end
+
+put '/students/:id' do
+  student = Student.get(params[:id])
+  student.update(params[:student])
+  redirect to("/students/#{student.id}")
+end
+
+delete '/students/:id' do
+  Student.get(params[:id]).destroy
+  redirect to('/students')
+end
